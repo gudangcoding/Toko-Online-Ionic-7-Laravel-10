990c8527f6abd57e4011b5829e908304cb2e80c7
nepriantodeveloper <neprianto@gmail.com>
2024-02-03 22:37:54 +0700

    navigasi dari whislist

--- src/app/home/whislist/whislist.page.ts
+++ src/app/home/whislist/whislist.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
 import { Helper } from 'src/provider/Helper';
 
@@ -9,20 +9,31 @@ import { Helper } from 'src/provider/Helper';
   styleUrls: ['./whislist.page.scss'],
 })
 export class WhislistPage implements OnInit {
-  products: any[] = [];
+  whistlist: any[] = [];
   hasil: any[] = [];
   searchTerm:any="";
 
   constructor(private cartService: CartService, private router: Router,private util:Helper) {
-    this.products = cartService.getCart('cart');
-    console.log('Total atas : ', this.products);
+    this.whistlist = cartService.getCart('whistlist');
+    console.log('Total atas : ', this.whistlist);
   }
 
   ngOnInit() {
   }
 
   searchByName(searchTerm: any) {
-    this.hasil = this.products.filter((product: any) => product.name.toLowerCase().includes(this.searchTerm.toLowerCase()));
+    this.hasil = this.whistlist.filter((product: any) => product.name.toLowerCase().includes(this.searchTerm.toLowerCase()));
     console.log(this.hasil);
+  }
+
+  detail(id:any) {
+
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        id: id,
+      },
+    };
+    this.router.navigate(['/detail-produk'], navigationExtras);
+
   }
 }
