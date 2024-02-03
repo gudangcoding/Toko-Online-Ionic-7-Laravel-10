764e64f1243f65942d058e45ea1edeae45c8de63
nepriantodeveloper <neprianto@gmail.com>
2024-02-04 01:05:27 +0700

    get user from storage

--- src/app/home/profil/profil.page.ts
+++ src/app/home/profil/profil.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.page.scss'],
 })
 export class ProfilPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  profilePhoto: any =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png';
+  user: any = [];
+  constructor() {
+    this.user = localStorage.getItem('member');
+    console.log(this.user);
+    
   }
 
+  ngOnInit() {}
+  edit() {}
+  logout() {}
 }
