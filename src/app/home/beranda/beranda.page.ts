7c3da16bb5aa7c1bddacf2fa13bfc264a4d3168c
nepriantodeveloper <neprianto@gmail.com>
2024-02-03 22:56:58 +0700

    infinity beranda

--- src/app/home/beranda/beranda.page.ts
+++ src/app/home/beranda/beranda.page.ts
@@ -11,9 +11,13 @@ import { RestApi } from 'src/provider/RestApi';
 })
 export class BerandaPage implements OnInit {
   searchTerm: string = '';
-  products: any[] = [];
   filteredProducts: any[] = [];
+  
+  products: any[] = [];
+  displayedProducts: any[] = [];
+  itemsPerPage: number = 10;
   currentPage: number = 1;
+  response:any=[];
 
   constructor(
     private api: RestApi,
@@ -21,7 +25,8 @@ export class BerandaPage implements OnInit {
     private router: Router,
     private util : Helper
   ) {
-    this.loadData(null);
+    // this.loadData(null);
+    this.loadMoreData();
   }
 
   ngOnInit() {}
@@ -39,6 +44,8 @@ export class BerandaPage implements OnInit {
     this.getProduct(this.currentPage).subscribe((res: any) => {
       this.util.dismissLoading();
       this.products = this.products.concat(res.products);
+      console.log('event ',event);
+      
       if (event) {
         event.target.complete();
       }
@@ -52,6 +59,30 @@ export class BerandaPage implements OnInit {
     });
   }
 
+  async loadMoreData(event?: any) {
+    const apiUrl = 'https://dummyjson.com/products';
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+
+    try {
+      // this.response = await this.http.get<any[]>(apiUrl).toPromise();
+      this.http.get(apiUrl).subscribe((res:any)=>{
+        this.response = res.products;
+        this.products = this.response.slice(startIndex, endIndex);
+        this.displayedProducts = [...this.displayedProducts, ...this.products];
+      });
+     
+
+      if (event) {
+        event.target.complete();
+      }
+
+      this.currentPage++;
+    } catch (error) {
+      console.error('Error fetching data from API:', error);
+    }
+  }
+  
   toSerach(cari:any) {
     let navigationExtras: NavigationExtras = {
       queryParams: {
