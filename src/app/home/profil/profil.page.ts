9192e0f219a6bca2afdc45beb7b8e75e41d51c60
nepriantodeveloper <neprianto@gmail.com>
2024-02-05 00:57:01 +0700

    upload foto profil

--- src/app/home/profil/profil.page.ts
+++ src/app/home/profil/profil.page.ts
@@ -1,4 +1,9 @@
+import { CartService } from 'src/app/services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestApi } from 'src/provider/RestApi';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 
 @Component({
   selector: 'app-profil',
@@ -6,16 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.page.scss'],
 })
 export class ProfilPage implements OnInit {
-  profilePhoto: any =
+  profilePhoto =
     'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png';
-  user: any = [];
-  constructor() {
-    this.user = localStorage.getItem('member');
-    console.log(this.user);
+  user: any ;
+  profil: any = [];
+
+  constructor(
+    private router:Router,
+    private api:RestApi,
+    private cartService: CartService,
+    ) {
+  this.user = cartService.getCart('member');
+    console.log(this.user.token);
     
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getUser();
+  }
+
+  getUser(){
+    this.api.getWithToken('user/profil/'+this.user.id,this.user.token).subscribe((res:any)=>{
+      console.log(this.user.token);
+      this.profil=res.data;
+    });
+  }
+
   edit() {}
-  logout() {}
+
+  logout() {
+    localStorage.removeItem('member');
+    this.router.navigateByUrl('login');
+  }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+    });
+
+    this.uploadPicture(image.dataUrl);
+  }
+
+  uploadPicture(imageData:any) {
+    const postData = { image: imageData,id:this.user.id };
+    this.api.post(postData,'user/uploadfoto').subscribe((res:any)=>{
+    })
+  }
 }
